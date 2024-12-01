201ce5dbfd48481d2aa2dcfde302cba597151248
vinr567 <OskoPoland@github.com>
2024-12-01 13:45:18 -0800

    Added up and downsampling blocks

--- paper.py
+++ paper.py
@@ -34,8 +34,54 @@ class MatrixAdder(nn.Module):
 # strid 2 3x3 conv block
 
 # Downsampling Block - allows us to use larger network for same computational cost
-# 3x3 conv on CxWxD matrix results in 9HWC^2 computations
-# Network benefits: 
+# C - number of filters (how many to use?)
+# 3x3 conv on CxWxH matrix results in 9HWC^2 computations
+# 3x3 conv with D downsample gives CDxW/DxH/D gives also 9HWC^2 comps
+# Network benefits: We want one pixel to have a larger receptive field (i.e. range of
+# data that the pixel affects) which can be done by stacking layers (computationally expensive)
+# downsampling by some factor D increases the receptive field by 2D
+# This block will take the input and per the paper the first conv is 9x9
+class DownsampleBlock(nn.Module):
+    def __init__(self):
+        super(DownsampleBlock, self).__init__()
+        self.downblock = nn.Sequential(
+            nn.Conv2d(in_channels=3,
+                      out_channels=3,
+                      kernel_size=(9,9),
+                      stride=2),
+            nn.Conv2d(in_channels=3,
+                      out_channels=3,
+                      kernel_size=(3,3),
+                      stride=2)
+        )
+        
+    def forward(self, x):
+        output = self.downblock(x)
+        return output
+
+# Upsampling Block
+# The paper proposes an upsampling process to create high resolution
+# results. The paper has options for a dynamic upsampling factor f. Instead
+# Instead of the factor we will be following the design the paper proposes
+# to use two stride = 1/2 convolution layers to upsample the image
+class UpsampleBlock(nn.Module):
+    def __init__(self):
+        super(UpsampleBlock, self).__init__()
+        self.upblock = nn.Sequential(
+            nn.Conv2d(in_channels=3,
+                      out_channels=3,
+                      kernel_size=(3,3),
+                      stride= 0.5),
+            # This is the last layer and per the paper will have a 9x9 kernel
+            nn.Conv2d(in_channels=3,
+                      out_channels=3,
+                      kernel_size=(9,9),
+                      stride= 0.5)
+        )
+    
+    def forward(self, x):
+        output = self.upblock(x)
+        return output
 
 # Residual block
 # temp = stored input imag
